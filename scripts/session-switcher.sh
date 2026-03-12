64b1b3fd600d8062eace17d2d75891735ba4fd7d
Saravenpi <saravenpi@tuta.io>
2026-03-12 23:05:13 +0100

    🐛 Fixed session switcher

--- scripts/session-switcher.sh
+++ scripts/session-switcher.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 current_session=$(tmux display-message -p '#S' 2>/dev/null)
+tmp_file=$(mktemp)
 
-selected=$(
-    tmux display-popup -w 60% -h 60% -E "
-        sessions=\$(tmux list-sessions -F '#{session_last_attached} #{session_name}' 2>/dev/null | sort -rn | cut -d' ' -f2-)
+tmux display-popup -w 60% -h 60% -E "
+    sessions=\$(tmux list-sessions -F '#{session_last_attached} #{session_name}' 2>/dev/null | sort -rn | cut -d' ' -f2-)
 
-        {
-            if [ -n '$current_session' ]; then
-                echo '$current_session'
-            fi
+    {
+        if [ -n '$current_session' ]; then
+            echo '$current_session'
+        fi
 
-            echo \"\$sessions\" | grep -v '^$current_session\$'
-        } | grep -v '^$' | fzf --prompt='Session > ' --reverse --print-query | tail -1
-    "
-)
+        echo \"\$sessions\" | grep -v '^$current_session\$'
+    } | grep -v '^$' | fzf --prompt='Session > ' --reverse --print-query | tail -1 > $tmp_file
+"
+
+selected=$(cat "$tmp_file" 2>/dev/null)
+rm -f "$tmp_file"
 
 [ -z "$selected" ] && exit 0
 
